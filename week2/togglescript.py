6b70a2e504ef157f05b56c0ec430229e6fbc45d8
pascal-maker <pascal-musa@hotmail.com>
2026-04-06 16:44:08 +0200

    refactor: improve button state detection logic and add inline documentation to togglescript.py

--- week2/togglescript.py
+++ week2/togglescript.py
@@ -1,34 +1,36 @@
-import time
 from RPi import GPIO
-GPIO.setmode(GPIO.BCM)
-led_pin = 17
-btn = 20
-GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(led_pin, GPIO.OUT)
-n_presses = 0
-led_state = 0
+import time
+
+GPIO.setmode(GPIO.BCM)#set the mode to BCM
+button = 20#define the button pin
+led_pin = 17#define the led pin
+GPIO.setup(button,GPIO.IN,pull_up_down=GPIO.PUD_UP)#set the button pin as input with pull up
+GPIO.setup(led_pin,GPIO.OUT)#set the led pin as output
+n_presses = 0#initialize the number of presses
+led_state = 0#initialize the led state
+
 GPIO.output(led_pin, led_state)
-previous_btn_state = GPIO.input(btn)
+
 try:
-    
+    previous_state = GPIO.input(button)#initialize the previous state
     while True:
-        btn_state = GPIO.input(btn)
-        if previous_btn_state == 1 and btn_state == 0:
-            led_state = not led_state
-            GPIO.output(led_pin, led_state)
-            n_presses = n_presses + 1
-            print("Button pressed {} times".format(led_state))
-            print('Pressed {} times'.format(n_presses))
-            time.sleep(0.2)
-        previous_btn_state = btn_state
-        time.sleep(0.01)
-except KeyboardInterrupt:
-    print("ctrl +c pressed")    
-finally:
-    print("Cleanup") 
-    GPIO.cleanup()   
-            
-#The script starts with the LED turned off and stores the initial button state in previous_btn_state.
-# Inside the loop, it keeps reading the current button state. When the button changes from released (1) to pressed (0), the script recognizes this as a real button press.
-# It then toggles the LED state, updates the LED output, and increases the press counter. 
-# Finally, it saves the current button state as the new previous state for the next loop.
+        current_state = GPIO.input(button)#get the current state
+        if current_state != previous_state:#check if the button state has changed
+            if current_state == GPIO.LOW: #check if the button is pressed
+                led_state = not led_state #toggle the led state
+                GPIO.output(led_pin, led_state)#output the led state
+                print('Button is pressed, led is now {}'.format(led_state))#print the led state
+                
+                n_presses += 1 #increment the number of presses
+                print('Number of presses: {}'.format(n_presses))#print the number of presses
+                time.sleep(0.05)#wait for 0.5 seconds
+            else:
+                print('Button is released')#print the button state
+            previous_state = current_state
+        time.sleep(0.01)        
+
+except KeyboardInterrupt:#when the user press ctrl+c
+    print("Exiting the program")
+finally:    
+    GPIO.cleanup()#cleanup the GPIO pins
+    
